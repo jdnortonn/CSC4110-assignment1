3c369de66fc769da2dd9f70027520843cdafde13
jdnortonn <hp9251@wayne.edu>
2025-09-05 07:08:01 -0400

    added author name

--- HelloWorld-1.cpp
+++ HelloWorld-1.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// author John Norton email hp9251@wayne.edu
 #include<iostream>  
   
 using namespace std; 
